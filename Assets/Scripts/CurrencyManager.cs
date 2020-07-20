5bf3217075813df5ddd5151d04501aa897a27035
Scratcher3004 <friedrich.p.steinkamp@gmail.com>
2020-07-20 21:34:43 +0200

    Griechische Deko Teil 1

---
+++ Assets/Scripts/CurrencyManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrencyManager : MonoBehaviour
+{
+    public static CurrencyManager instance;
+
+    private int money;
+    
+    void Awake()
+    {
+        if (instance)
+            Destroy(this);
+        else
+            instance = this;
+    }
+
+    public void AddMoney(int m)
+    {
+        money += m;
+    }
+
+    public bool Purchase(int cost)
+    {
+        if (cost > money)
+            return false;
+        money -= cost;
+        return true;
+    }
+}
---
+++ Assets/Scripts/CurrencyOnDeath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Core.Health;
+using UnityEngine;
+
+[RequireComponent(typeof(DamageableBehaviour))]
+public class CurrencyOnDeath : MonoBehaviour
+{
+    public int money = 1;
+    
+    private void Start()
+    {
+        GetComponent<DamageableBehaviour>().configuration.died += Death;
+    }
+
+    private void Death(HealthChangeInfo none)
+    {
+        CurrencyManager.instance.AddMoney(money);
+    }
+}
