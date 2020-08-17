547ef0a11d13fabd50314b7f88ddc1cb6a55f56c
Scratcher3004 <friedrich.p.steinkamp@gmail.com>
2020-08-17 14:01:23 +0200

    Egypt changes

---
+++ Assets/PeacefulMode/GameObjectSwapInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace PeacefulMode
+{
+    [Serializable]
+    public class GameObjectSwapInfo
+    {
+        public GameObject from;
+        public GameObject to;
+    }
+}
---
+++ Assets/PeacefulMode/PeacefulMode.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace PeacefulMode
+{
+    public class PeacefulMode : MonoBehaviour
+    {
+        public static bool disablePeaceful = false;
+        public bool overrideDisablePeaceful = false;
+        public bool disablePeacefulOverride = true;
+        
+        public List<GameObjectSwapInfo> swap = new List<GameObjectSwapInfo>();
+
+        public static PeacefulMode singleton;
+        
+        void Start()
+        {
+            if (singleton != null)
+            {
+                Debug.LogError("There can only be one peaceful instance in the scene!");
+                Destroy(this);
+                return;
+            }
+            
+            singleton = this;
+            
+            if (disablePeaceful && !overrideDisablePeaceful || overrideDisablePeaceful && disablePeacefulOverride)
+            {
+                enabled = false;
+                return;
+            }
+            
+            Debug.Log("Enabled Peaceful Mode!");
+        }
+
+        public static void NormalToPeaceful(GameObject original)
+        {
+            
+            
+            //var swapped = 
+        }
+    }
+}
